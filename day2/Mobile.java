f2eb6cfd81fde70e08f994ac194d549529774f0e
NIKHIL STEPHEN RAJ B C <61453559+nikhilstep@users.noreply.github.com>
2023-05-08 21:01:00 +0530

    Add files via upload

---
+++ day2/Mobile.java
@@ -0,0 +1,31 @@
+package week1.day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String brandName="Samsung";
+		System.out.println(brandName);
+		
+		char mobileLogo='S';
+		System.out.println(mobileLogo);
+		
+		short noOfMobilePiece=3;
+		System.out.println(noOfMobilePiece);
+		
+		int modelNo=78957;
+		System.out.println(modelNo);
+		
+		long mobileImeiNo=329873278784l;
+		System.out.println(mobileImeiNo);
+		
+		float mobilePrice=7000f;
+		System.out.println(mobilePrice);
+		
+		boolean isDamaged=false;
+		System.out.println(isDamaged);
+
+	}
+
+}
---
+++ day2/TwoWheeler.java
@@ -0,0 +1,28 @@
+package week1.day2;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int noOfWheels=2;
+		System.out.println(noOfWheels);
+		
+		short noOfMirrors=2;
+		System.out.println(noOfMirrors);
+		
+		long chassisNo=1237261l;
+		System.out.println(chassisNo);
+		
+		boolean isPunctured=false;
+		System.out.println(isPunctured);
+		
+		String bikeName="Honda";
+		System.out.println(bikeName);
+		
+		double runningKm=1500;
+		System.out.println(runningKm);
+
+	}
+
+}
